c0da6cf837e07f7b6f63a02b548f59201c19ee50
Paul Nakonieczny <paulnakonieczny@Pauls-MacBook-Pro.local>
2020-12-09 20:32:18 -0500

    Complete day 3

--- day3/day3.py
+++ day3/day3.py
@@ -4,9 +4,18 @@ while(True):
     newline = data_file.readline()
     if newline == '':
         break
-    data.append(newline)
+    data.append(newline.rstrip())
     
-x_pos, x_max = 0, len(data)
-y_pos = 0
+x_pos, x_max = 0, len(data[0])
+y_pos, y_max = 0, len(data)
+trees = 0
 
+while y_pos < y_max:
+    if data[y_pos][x_pos] == '#':
+        trees += 1
 
+    x_pos += 1
+    x_pos %= x_max
+    y_pos += 2
+
+print(trees)
